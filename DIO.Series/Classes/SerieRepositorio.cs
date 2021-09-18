5f835f069c35ed03d6d1792e796a44fececbfc24
Josan Johnata <josanjohnata@gmail.com>
2021-09-18 17:06:18 -0300

    Adiciona implementaçoes

---
+++ DIO.Series/Classes/SerieRepositorio.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using DIO.Series.Interfaces;
+
+namespace DIO.Series
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        public void Atualizar(int id, Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<serie> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+        public Serie RetornaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
